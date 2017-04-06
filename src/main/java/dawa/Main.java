cd1df97c280062c3e2ac7efb1d25f6ed27c2caea
cout970 <thecout970@gmail.com>
2017-04-06 19:06:52 +0200

    Added gradle

---
+++ src/main/java/dawa/Main.java
@@ -0,0 +1,11 @@
+package dawa;
+
+/**
+ * Created by cout970 on 2017/04/06.
+ */
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println("Hello world");
+    }
+}
