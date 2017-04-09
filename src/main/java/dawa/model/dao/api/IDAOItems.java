8c38805f5ef95c52b81cb5ee52ae610441dd566c
petrusboniatus <pedrobuenisimo@gmail.com>
2017-04-09 21:41:19 +0200

    Acabado DAOItems, IDAOItems comentada con sus test funcionado

--- src/main/java/dawa/model/dao/api/IDAOItems.java
+++ src/main/java/dawa/model/dao/api/IDAOItems.java
@@ -7,11 +7,27 @@ import dawa.model.VOs.*;
  */
 public interface IDAOItems {
 
+    /**
+     * @param param options of the search
+     * @return Items that have the values described on param
+     */
     ItemList searchItems(ItemSearchParameter param);
 
+    /**
+     * Insert an item, throw Illegal argument exception when it already exist
+     * @param item item to insert
+     */
     void insertItem(Item item);
 
+    /**
+     * Save the values of the passed argument on the DB
+     * @param item item with updated values
+     */
     void updateItem(Item item);
 
+    /**
+     * Delete the item witch item.id is equal to the item parameter
+     * @param item item to delete
+     */
     void deleteItem(Item item);
 }
