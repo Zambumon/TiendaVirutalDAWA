b69ec0c58078f3473625202728f28f81c23e5313
cout970 <thecout970@gmail.com>
2017-04-12 16:29:21 +0200

    Added all actions as templates

---
+++ src/main/java/dawa/controller/actions/AddItem.java
@@ -0,0 +1,23 @@
+package dawa.controller.actions;
+
+import dawa.controller.Action;
+import dawa.controller.Dispatcher;
+import dawa.controller.ShopController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by cout970 on 2017/04/12.
+ */
+public class AddItem extends Action {
+
+    public AddItem(ShopController controller, Dispatcher dispatcher, String path) {
+        super(controller, dispatcher, path);
+    }
+
+    @Override
+    public void doAction(HttpServletRequest req, HttpServletResponse res) {
+
+    }
+}
---
+++ src/main/java/dawa/controller/actions/EditStock.java
@@ -0,0 +1,23 @@
+package dawa.controller.actions;
+
+import dawa.controller.Action;
+import dawa.controller.Dispatcher;
+import dawa.controller.ShopController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by cout970 on 2017/04/12.
+ */
+public class EditStock extends Action {
+
+    public EditStock(ShopController controller, Dispatcher dispatcher, String path) {
+        super(controller, dispatcher, path);
+    }
+
+    @Override
+    public void doAction(HttpServletRequest req, HttpServletResponse res) {
+
+    }
+}
